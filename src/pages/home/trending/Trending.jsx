7c5c8e3b5f152248c90f095078e5372cd489a22e
Abhishek Pratap Singh <abhishekpratap.gh@gmail.com>
2023-06-15 18:45:35 +0530

    Carousel and Switch tabs created

--- src/pages/home/trending/Trending.jsx
+++ src/pages/home/trending/Trending.jsx
@@ -1,8 +1,27 @@
-import React from "react";
+import React, { useState } from "react";
 import ContentWrapper from "../../../components/contentWrapper/ContentWrapper";
+import SwitchTabs from "../../../components/switchTabs/SwitchTabs";
+import useFetch from "../../../hooks/useFetch";
+import Carousel from "../../../components/carousel/Carousel";
 
 const Trending = () => {
-  return <div></div>;
+  const [endpoint, setEndpoint] = useState("day");
+  const { data, loading } = useFetch(`/trending/all/${endpoint}`);
+  const onTabChange = (tab) => {
+    setEndpoint(tab.toLowerCase());
+    // or we can use this instead
+    // setEndpoint(tab== "day" ? "day" : "week");
+  };
+
+  return (
+    <div className="carouselSection">
+      <ContentWrapper>
+        <span className="carouselTitle">Trending</span>
+        <SwitchTabs data={["day", "Week"]} onTabChange={onTabChange} />
+      </ContentWrapper>
+      <Carousel data={data?.results} loading={loading} />
+    </div>
+  );
 };
 
 export default Trending;
