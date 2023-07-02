5b0a5d7b0e0525d8e941137d77b779fb154e377e
Abhishek Pratap Singh <arabhishek442@gmail.com>
2023-07-02 21:04:40 +0530

    Details page created and add the section of similar and recommdeations in it and search componemt created

--- src/pages/searchResult/SearchResult.jsx
+++ src/pages/searchResult/SearchResult.jsx
@@ -1,7 +1,18 @@
-import React from "react";
+import React ,{useEffect,useState} from "react";
+import { useParams } from "react-router-dom";
+import InfiniteScroll from "react-infinite-scroll-component";
+import { fetchDataFromAPI } from "../../utils/api";
+import ContentWrapper from "../../components/contentWrapper/ContentWrapper";
+// import MovieCard from "../../components/movirCard/MovieCard"
+// import Spinner from "../"
+import noResults from "../../assets/no-results.png"
 import "./style.scss";
 
 const SearchResult = () => {
+  const [data ,setData] = useState(null)
+  const [pageNum ,setPageNum] = useState(1)
+  const [loading ,setLoading] = useState(false)
+  const {query} = useParams()
   return <div></div>;
 };
 
